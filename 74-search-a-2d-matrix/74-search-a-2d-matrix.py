da9ffd46a6eb9179215daf398a41db6a92f764f6
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-06-02 23:23:21 -0400

    Time: 42 ms (43.10%), Space: 13.9 MB (51.41%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        
+        
+        for i in range(len(matrix)):
+            #print(matrix[i][0])
+            if matrix[i][-1]>=target or len(matrix)==1:
+                print(matrix[i][-1])
+                if matrix[i][0]==target:
+                    return True
+                for j in range(len(matrix[0])):
+             #       print(matrix[i-1][j])
+                    if matrix[i-1][j]==target or matrix[i][j]==target:
+                        return True
+                    
+        return False
