5cd83b9d1e249f876e6789a0f8a9f887d92292ff
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-06-02 23:26:23 -0400

    Time: 30 ms (83.17%), Space: 13.7 MB (93.74%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -10,9 +10,7 @@ class Solution(object):
         for i in range(len(matrix)):
             #print(matrix[i][0])
             if matrix[i][-1]>=target:
-                print(matrix[i][-1])
-                if matrix[i][0]==target:
-                    return True
+
                 for j in range(len(matrix[0])):
              #       print(matrix[i-1][j])
                     if matrix[i][j]==target:
