1357604ba88368a963ed48de049fdc5cd655979c
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-06-10 11:17:24 -0400

    Time: 202 ms (83.98%), Space: 0B (100.00%) - LeetHub

--- 571-find-median-given-frequency-of-numbers/571-find-median-given-frequency-of-numbers.sql
+++ 571-find-median-given-frequency-of-numbers/571-find-median-given-frequency-of-numbers.sql
@@ -1,7 +1,16 @@
 # Write your MySQL query statement below
 
 
-select  avg(n.num) median
-from Numbers n
-where n.Frequency >= abs((select sum(Frequency) from Numbers where num<=n.num) -
-                         (select sum(Frequency) from Numbers where num>=n.num))
+WITH cte AS (SELECT floor((sum(frequency) + 1) / 2) AS m1
+    , FLOOR((SUM(frequency) + 2) / 2) AS m2
+FROM numbers )
+    , cte2 AS (select num
+    , SUM(frequency) OVER (ORDER BY num ROWS UNBOUNDED PRECEDING) AS presum
+FROM numbers )
+    , n1 AS ( SELECT num
+FROM cte2 WHERE presum >= (SELECT m1 FROM cte) LIMIT 1)
+    , n2 AS ( SELECT num 
+FROM cte2 WHERE presum >= (SELECT m2 FROM cte) LIMIT 1)
+
+SELECT AVG(num) AS median 
+FROM (select * FROM n1 UNION SELECT * FROM n2) t
