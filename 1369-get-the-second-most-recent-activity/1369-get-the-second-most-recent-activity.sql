f456b8f1179b8ca9d72951904118e96955ab0500
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-06-13 15:06:59 -0400

    Time: 563 ms (13.74%), Space: 0B (100.00%) - LeetHub

--- 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
+++ 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
 
-with v1 as(
-select *,dense_rank() over(partition by username order by startDate desc) as rn from useractivity)
-select username,activity,startDate,endDate from v1 where rn=2
-union
-select username,activity,startDate,endDate from useractivity where username in(
-select username from useractivity group by username having count(*) =1
-)
+# with v1 as(
+# select *,dense_rank() over(partition by username order by startDate desc) as rn from useractivity)
+# select username,activity,startDate,endDate from v1 where rn=2
+# union
+# select username,activity,startDate,endDate from useractivity where username in(
+# select username from useractivity group by username having count(*) =1
+# )
+
+select username, activity, startDate, endDate
+from (
+select *, count(activity) over(partition by username)cnt, 
+ROW_NUMBER() over(partition by username order by startdate desc) n from UserActivity) tbl
+where n=2 or cnt<2
