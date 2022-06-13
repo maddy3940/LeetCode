4c1cf9178e059bc0a8f4287270beec5ce9c21907
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-06-13 15:07:20 -0400

    Time: 554 ms (14.48%), Space: 0B (100.00%) - LeetHub

--- 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
+++ 1369-get-the-second-most-recent-activity/1369-get-the-second-most-recent-activity.sql
@@ -10,6 +10,6 @@
 
 select username, activity, startDate, endDate
 from (
-select *, count(activity) over(partition by username)cnt, 
+select *, count(*) over(partition by username)cnt, 
 ROW_NUMBER() over(partition by username order by startdate desc) n from UserActivity) tbl
 where n=2 or cnt<2
