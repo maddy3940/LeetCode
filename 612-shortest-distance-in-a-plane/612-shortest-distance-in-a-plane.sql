2a0f9d473c2fa6246759ce5d2442d79cb94d8f00
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-14 13:49:28 -0400

    Time: 220 ms (80.56%), Space: 0B (100.00%) - LeetHub

---
+++ 612-shortest-distance-in-a-plane/612-shortest-distance-in-a-plane.sql
@@ -0,0 +1,8 @@
+SELECT
+    ROUND(SQRT(MIN((POW(p1.x - p2.x, 2) + POW(p1.y - p2.y, 2)))),2) AS shortest
+FROM
+    point2d p1
+        JOIN
+    point2d p2 ON (p1.x <= p2.x AND p1.y < p2.y)
+        OR (p1.x <= p2.x AND p1.y > p2.y)
+        OR (p1.x < p2.x AND p1.y = p2.y)
