3706b9aafc191375d0e51b44e75c1c2b7f31855d
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-17 17:14:12 -0400

    Time: 347 ms (59.69%), Space: 0B (100.00%) - LeetHub

--- 1543-fix-product-name-format/1543-fix-product-name-format.sql
+++ 1543-fix-product-name-format/1543-fix-product-name-format.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
 
-select trim(lower(product_name)) as product_name,date_format(sale_date,"%Y-%m") as sale_date,count(*) as total from sales group by trim(lower(product_name)),date_format(sale_date,"%Y-%m") order by 1 asc,2 asc
+# select trim(lower(product_name)) as product_name,date_format(sale_date,"%Y-%m") as sale_date,count(*) as total from sales group by trim(lower(product_name)),date_format(sale_date,"%Y-%m") order by 1 asc,2 asc
+SELECT LOWER(TRIM(product_name)) product_name, DATE_FORMAT(sale_date, "%Y-%m") sale_date, count(sale_id) total
+FROM sales
+GROUP BY 1, 2
+ORDER BY 1, 2
 
 
 
