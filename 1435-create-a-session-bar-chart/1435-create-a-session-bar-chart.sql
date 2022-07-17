db82079a5bea5dac546366e502d2a75684a72ce6
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-17 16:49:50 -0400

    Time: 525 ms (40.27%), Space: 0B (100.00%) - LeetHub

---
+++ 1435-create-a-session-bar-chart/1435-create-a-session-bar-chart.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+
+with v1 as(
+select bin,count(bin) as total from(
+select case when (duration/60<5) then "[0-5>"
+when (duration/60>=5 and duration/60<10) then "[5-10>"
+when (duration/60>=10 and duration/60<=15) then "[10-15>"
+when (duration/60>=15) then "15 or more"
+end as bin
+from sessions)a group by bin),
+v2 as(
+select "[0-5>" as bin,0 as total
+union
+select "[5-10>" as bin,0 as total
+union
+select "[10-15>" as bin,0 as total
+union
+select "15 or more" as bin,0 as total)
+select * from v1 
+union 
+select * from v2 where bin not in (select bin from v1)
+
