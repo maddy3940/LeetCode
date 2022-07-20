4f16dd4f281e5bf7feade09a8bd03e88d8e51c1f
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-19 22:50:05 -0400

    Time: 1154 ms (22.76%), Space: 0B (100.00%) - LeetHub

--- 1939-users-that-actively-request-confirmation-messages/1939-users-that-actively-request-confirmation-messages.sql
+++ 1939-users-that-actively-request-confirmation-messages/1939-users-that-actively-request-confirmation-messages.sql
@@ -1,13 +1,16 @@
 # Write your MySQL query statement below
 
-with v1 as(
-select user_id, timestampdiff(second,time_stamp,lead(time_stamp,1,"2031-01-01") over(partition by user_id order by time_stamp asc)) as lead_time_stamp from confirmations
-)
-select distinct user_id from v1 where lead_time_stamp<=86400
+# with v1 as(
+# select user_id, timestampdiff(second,time_stamp,lead(time_stamp,1,"2031-01-01") over(partition by user_id order by time_stamp asc)) as lead_time_stamp from confirmations
+# )
+# select distinct user_id from v1 where lead_time_stamp<=86400
 
-# select user_id, time_stamp,lead(time_stamp,1,"2031-01-01") over(partition by user_id order by time_stamp asc) as lead_time_stamp, timestampdiff(second,time_stamp,lead(time_stamp,1,"2031-01-01") over(partition by user_id order by time_stamp asc)) as lea from confirmations
-
-
+select
+distinct c.user_id
+from confirmations c
+join confirmations d
+on c.user_id=d.user_id and c.time_stamp>d.time_stamp and 
+timestampdiff(second, d.time_stamp,c.time_stamp)<=86400
 
 
 
