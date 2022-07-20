4981f4c3c7b7640232c66b053295b58e92c593f5
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-19 23:09:13 -0400

    Time: 3518 ms (5.12%), Space: 0B (100.00%) - LeetHub

--- 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
+++ 2082-the-number-of-rich-customers/2082-the-number-of-rich-customers.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-select count(*) as rich_count from (
-select customer_id,count(*) from store where amount>500 group by customer_id) a
 
 
+SELECT COUNT(DISTINCT customer_id) AS rich_count FROM Store WHERE amount > 500
 
 
 
