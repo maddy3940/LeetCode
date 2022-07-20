b7716107447b4844d94b580737c5f2eb75a8746a
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-19 23:01:25 -0400

    Time: 1580 ms (15.89%), Space: 0B (100.00%) - LeetHub

--- 2072-the-winner-university/2072-the-winner-university.sql
+++ 2072-the-winner-university/2072-the-winner-university.sql
@@ -1,5 +1,15 @@
 # Write your MySQL query statement below
 
-select case when (select count(*) from NewYork where score>=90)>(select count(*) from California where score>=90) then "New York University"
-when (select count(*) from NewYork where score>=90)<(select count(*) from California where score>=90) then "California University"
-else "No Winner" end as winner
+# select case when (select count(*) from NewYork where score>=90)>(select count(*) from California where score>=90) then "New York University"
+# when (select count(*) from NewYork where score>=90)<(select count(*) from California where score>=90) then "California University"
+# else "No Winner" end as winner
+
+select
+case when t.ny_score > t.cal_score then 'New York University'
+when t.ny_score < t.cal_score then 'California University'
+else 'No Winner'
+end as winner
+from
+(
+select (select count(student_id) from NewYork where score >= 90 ) as ny_score , (select count(student_id) from California where score >= 90) as cal_score
+) t
