9847826b14237144bf1f9136de9203c6770e4119
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-07-26 20:28:40 -0400

    Time: 612 ms (43.78%), Space: 0B (100.00%) - LeetHub

---
+++ 1747-leetflex-banned-accounts/1747-leetflex-banned-accounts.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+with v1 as(
+select account_id, ip_address,lead(ip_address) over(partition by account_id order by login asc) as next_ip, login, lead(login,1,'2050-01-01') over(partition by account_id order by login asc) as next_login, logout from loginfo)
+select distinct account_id from(
+select account_id, case when next_login<=logout and ip_address!=next_ip then 1 else 0 end as check1 from v1) a where check1=1
+
+
+
+
+
