05472c6e4742ae48606ad1c6a021bef5f56de625
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2022-09-15 11:35:56 -0400

    Time: 2014 ms (5.03%), Space: 0B (100.00%) - LeetHub

--- 1949-strong-friendship/1949-strong-friendship.sql
+++ 1949-strong-friendship/1949-strong-friendship.sql
@@ -1,11 +1,26 @@
 # Write your MySQL query statement below
 
-with v1 as(
-select user1_id,user2_id from friendship union select user2_id,user1_id from friendship),
-v2 as(
-select f1.user1_id as u11,f2.user1_id as u12,f1.user2_id as u21,f2.user2_id as u22 from v1 f1, v1 f2 where f1.user1_id<>f2.user1_id and f1.user2_id=f2.user2_id and f1.user1_id<f2.user1_id)
-select u11 as user1_id, u12 as user2_id, count(*) as common_friend from v2 where (u11,u12) in (select user1_id,user2_id from friendship) group by 1,2 having count(*)>=3 order by 1,2,3
+# with v1 as(
+# select user1_id,user2_id from friendship union select user2_id,user1_id from friendship),
+# v2 as(
+# select f1.user1_id as u11,f2.user1_id as u12,f1.user2_id as u21,f2.user2_id as u22 from v1 f1, v1 f2 where f1.user1_id<>f2.user1_id and f1.user2_id=f2.user2_id and f1.user1_id<f2.user1_id)
+# select u11 as user1_id, u12 as user2_id, count(*) as common_friend from v2 where (u11,u12) in (select user1_id,user2_id from friendship) group by 1,2 having count(*)>=3 order by 1,2,3
 
-
+with cte as(select user1_id,user2_id from friendship
+            union 
+            select user2_id,user1_id from friendship)
+            
+select c1.user1_id
+      ,c2.user1_id as user2_id
+	  ,count(*) as common_friend 
+from cte as c1 
+     join 
+	 cte as c2
+on c1.user1_id<c2.user1_id 
+   and 
+   c1.user2_id=c2.user2_id
+where (c1.user1_id,c2.user1_id) in (select * from friendship)
+group by 1,2
+having count(*)>=3
 
 
