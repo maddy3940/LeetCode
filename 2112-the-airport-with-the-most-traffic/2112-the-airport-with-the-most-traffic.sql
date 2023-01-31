fac6433672826f015e993aae952276f9c1120ce0
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-01-31 18:31:25 -0500

    Time: 777 ms (42.29%), Space: 0B (100.00%) - LeetHub

--- 2112-the-airport-with-the-most-traffic/2112-the-airport-with-the-most-traffic.sql
+++ 2112-the-airport-with-the-most-traffic/2112-the-airport-with-the-most-traffic.sql
@@ -2,8 +2,8 @@
 
 with v1 as(
 select airport_id, sum(tot) as tot from (
-select departure_airport as airport_id, sum(flights_count)as tot from flights group by 1
-union 
-select arrival_airport as airport_id, sum(flights_count)as tot from flights group by 1
+select departure_airport as airport_id, flights_count as tot from flights 
+union all
+select arrival_airport as airport_id, flights_count as tot from flights 
 ) a group by 1)
 select airport_id from v1 where tot = (select max(tot) from v1)
