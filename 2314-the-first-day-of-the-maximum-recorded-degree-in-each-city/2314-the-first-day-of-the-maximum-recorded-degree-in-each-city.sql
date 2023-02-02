f4b1d491048392c7bb03d5ab616e0799f3759952
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-02-02 16:13:06 -0500

    Time: 986 ms (44.64%), Space: 0B (100.00%) - LeetHub

--- 2314-the-first-day-of-the-maximum-recorded-degree-in-each-city/2314-the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
+++ 2314-the-first-day-of-the-maximum-recorded-degree-in-each-city/2314-the-first-day-of-the-maximum-recorded-degree-in-each-city.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-select city_id, min(day) as day, degree from (
+select city_id, day,degree from (
 select city_id, day,degree, dense_rank() over (partition by city_id order by degree desc,day asc) as max_deg from weather) a
-where max_deg =1 group by 1,3 order by 1 asc
+where max_deg =1 order by 1 asc
 
