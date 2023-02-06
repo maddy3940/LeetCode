d83558ed7b24b8e0f5d89d7ef40f0cd87e0b15f1
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-02-06 13:33:52 -0500

    Time: 75 ms (29.09%), Space: 14 MB (99.61%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,16 +1,19 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        
-        
-        a = nums[:n]
-        b = nums[n:]
-        
-        x=y=0
-        for i in range(2*n):
-            if i%2==0:
-                nums[i]=a[x]
-                x+=1
-            else:
-                nums[i]=b[y]
-                y+=1
-        return(nums)
+        # Store each y(i) with respective x(i).
+        for i in range(n, 2 * n):
+            secondNum = nums[i] << 10
+            nums[i - n] |= secondNum
+
+        # '0000000000 1111111111' in decimal.
+        allOnes = int(pow(2, 10)) - 1
+
+        # We will start putting all numbers from the end, 
+        # as they are empty places.
+        for i in range(n - 1, -1, -1):
+            # Fetch both the numbers from the current index.
+            secondNum = nums[i] >> 10
+            firstNum = nums[i] & allOnes
+            nums[2 * i + 1] = secondNum
+            nums[2 * i] = firstNum
+        return nums
