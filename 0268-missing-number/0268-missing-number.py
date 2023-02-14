e55c5289c45f8192adcdd789d46e9c243bcb03b5
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-02-13 20:54:50 -0500

    Time: 125 ms (96.86%), Space: 15.2 MB (32.00%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        nums=sorted(nums)
-        for i,val in enumerate(nums):
-            if i!=val:
-                return i
-        
-        return len(nums)
+        expected_sum = len(nums)*(len(nums)+1)//2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
