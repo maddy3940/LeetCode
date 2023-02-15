47b092e5bc40b6d02a8d39525db961ee1f0a27ac
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-02-14 19:40:54 -0500

    Time: 152 ms (38.13%), Space: 16.7 MB (82.99%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        d={}
+        for i in range(len(nums)):
+           
+            d[nums[i]]=d.get(nums[i],0)+1
+            
+        for k,v in d.items():
+            if v==1:
+                return k
+            
+            
+            
+            
