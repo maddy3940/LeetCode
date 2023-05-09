5b76855ccccff4e6a86a06a52171b03ce5e98247
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-05-09 00:04:04 -0400

    Time: 1523 ms (70.10%), Space: 0B (100.00%) - LeetHub

--- 1767-find-the-subtasks-that-did-not-execute/1767-find-the-subtasks-that-did-not-execute.sql
+++ 1767-find-the-subtasks-that-did-not-execute/1767-find-the-subtasks-that-did-not-execute.sql
@@ -12,3 +12,13 @@ FROM id
 LEFT JOIN Executed e ON id.task_id = e.task_id AND id.subtask_id = e.subtask_id 
 WHERE e.subtask_id IS NULL 
 AND id.subtasks_count >= id.subtask_id
+
+
+
+
+# with RECURSIVE recursive_cte as(
+#     select distinct task_id, 1 as subtask_id  from tasks
+#     union all
+#     select r.task_id, r.subtask_id+1 as subtask_id from recursive_cte r, tasks t where t.task_id=r.task_id and r.subtask_id+1<= t.subtasks_count 
+# )
+# select r.* from recursive_cte r where concat(r.task_id,r.subtask_id) not in (select concat(r.task_id,r.subtask_id) from executed r) order by 1,2
