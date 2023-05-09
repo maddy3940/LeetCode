587be6fe6a00f47e636ebee39ab3a5be207c430d
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-05-09 16:38:46 -0400

    Time: 59 ms (28.48%), Space: 16.3 MB (7.13%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -21,3 +21,16 @@ class Solution:
         print(res)
 
         return res
+
+        # total = 0
+        # i = 0
+        # while i < len(s):
+        #     # If this is the subtractive case.
+        #     if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+        #         total += values[s[i + 1]] - values[s[i]]
+        #         i += 2
+        #     # Else this is NOT the subtractive case.
+        #     else:
+        #         total += values[s[i]]
+        #         i += 1
+        # return total
