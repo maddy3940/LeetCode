2160f784fcf4af4f86b5a09c23767484f2924592
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-05-10 15:41:27 -0400

    Time: 44 ms (14.24%), Space: 16.4 MB (12.57%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    memo = {}
+    def climbStairs(self, n: int) -> int:
+
+        if n==1:
+            return 1
+        
+        dp={}
+        
+        dp[1]=1
+        dp[2]=2
+        
+        
+        for i in range(3,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+            
+            
+        return dp[n]
+            
+            
+            
+            
+            
