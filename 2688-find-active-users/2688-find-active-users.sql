50f166648953867b0787e80d0be368ad07ed0968
Mahadevan Iyer <44323045+maddy3940@users.noreply.github.com>
2023-05-14 19:51:53 -0400

    Time: 1627 ms (100.00%), Space: 0B (100.00%) - LeetHub

--- 2688-find-active-users/2688-find-active-users.sql
+++ 2688-find-active-users/2688-find-active-users.sql
@@ -3,8 +3,4 @@
 with cte0 as(
 select user_id, datediff(lead(created_at) over(partition by user_id order by created_at asc),created_at) as diff from users
     )
-    
-# select * from cte0
-
-
 select distinct user_id from cte0 where diff<=7
